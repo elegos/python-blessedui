c2862c2b57ee74c1a9b94f43fbb1adf2b3342d86
Giacomo "Mr. Wolf" Furlan <giacomo.furlan@fastwebnet.it>
2020-10-30 14:03:09 +0100

    Changelog + deploy

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.0.1] - 2020-10-30
+Alpha version. Limited tile sets.
+### Added
+- BlessedUI class
+- TileSubject generic class
+- Tile abstract class
+- tiles.TextTile class (no scrolling features)
+- tiles.SplitTile class
+- Basic README.md file
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blessedui",
+    license="LGPLv3",
+    version="0.0.1",
+    author="Giacomo Furlan",
+    author_email="opensource@giacomofurlan.name",
+    description="Graphic library based on blessed to create CLI interfaces at ease",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elegos/python-blessedui",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: LGPLv3 License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    install_requires=[
+        'blessed>=1.17.11'
+    ],
+    python_requires='>=3.6',
+)
